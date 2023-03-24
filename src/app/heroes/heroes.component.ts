111ec49bb07bbdd6d7ee3930b6484f67ce8de7b9
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2023-03-24 10:27:27 -0300

    feat: Adicionando lista de heróis e diretrizes no componente heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { heroesList } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
 
 
   hero: Hero = {
     id: 1,
     name:'Wolverine'
+  };
+
+  // Lista de heróis sendo importados
+
+  heroes = heroesList;
+
+  // Função click de selecionar um herói
+
+  selectHero?: Hero;
+
+  onSelect(hero: Hero) : void{
+    this.selectHero = hero;
   }
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,29 @@
+import { Hero } from "./hero.model";
+
+export const heroesList : Hero[] = [
+  {
+    id: 1,name :'Wolverine'
+  },
+  {
+    id: 2,name :'Homem-Aranha'
+  },
+  {
+    id: 3,name :'Hulk'
+  },
+  {
+    id: 4,name :'Thor'
+  },
+  {
+    id: 5,name :'Homem de Ferro'
+  },
+  {
+    id: 6,name :'Capitão América'
+  },
+  {
+    id: 7,name :'Pantera Negra'
+  },
+  {
+    id: 8,name :'Viúva Negra'
+  },
+
+];
